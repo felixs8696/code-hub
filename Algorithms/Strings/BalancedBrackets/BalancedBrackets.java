d31fe346076b91c163c60ee296bd9f9ccbfabd1c
Felix Su <felixs8696@berkeley.edu>
2016-12-03 05:20:38 -0800

    finished balanced brackets and templating in java

---
+++ Algorithms/Strings/BalancedBrackets/BalancedBrackets.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class BalancedBrackets{
+  HashSet<Character> openBrackets;
+  HashSet<Character> closedBrackets;
+
+  // Edge Cases: Pop from empty stack
+  public BalancedBrackets() {
+    openBrackets = new HashSet<Character>();
+    openBrackets.add('(');
+    openBrackets.add('{');
+    openBrackets.add('[');
+
+    closedBrackets = new HashSet<Character>();
+    closedBrackets.add(')');
+    closedBrackets.add('}');
+    closedBrackets.add(']');
+  }
+
+  private boolean isMatch(char open, char closed) {
+    if ((open == '(' && closed == ')') || (open == '{' && closed == '}') || (open == '[' && closed == ']'))
+      return true;
+    return false;
+  }
+
+  public boolean isBalanced(String str) {
+    Stack<Character> stack = new Stack<Character>();
+    for (int i = 0; i < str.length(); i++) {
+      char c = str.charAt(i);
+      if (openBrackets.contains(c)) {
+        stack.push(c);
+      } else if (closedBrackets.contains(c)) {
+        if (stack.isEmpty()) return false;
+        char prev = stack.pop();
+        if (!isMatch(prev, c)) return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new FileReader("io/tests.txt"));
+    BufferedWriter bw = new BufferedWriter(new FileWriter("io/solution.txt"));
+
+    BalancedBrackets bb = new BalancedBrackets();
+    boolean debug = true;
+
+    String test;
+    while ( (test = br.readLine()) != null ) {
+      String solution = String.valueOf(bb.isBalanced(test));
+      if (debug) solution +=  ": '" + test + "'";
+      bw.write(solution);
+      bw.newLine();
+    }
+
+    br.close();
+    bw.close();
+  }
+}
---
+++ Templates/Java/BufferedIOTemplate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class BufferedIOTemplate {
+
+  static String solve(String str) {
+    return "solution";
+  }
+
+  public static void main(String[] args) throws IOException, FileNotFoundException {
+    BufferedReader br = new BufferedReader(new FileReader("io/tests.txt"));
+    BufferedWriter bw = new BufferedWriter(new FileWriter("io/solution.txt"));
+    boolean debug = true;
+
+    BufferedIOTemplate classInstance = new BufferedIOTemplate(); // If non-static methods
+
+    String test;
+    while ( (test = br.readLine()) != null ) {
+      String solution = String.valueOf(solve(test));
+      if (debug) solution +=  ": '" + test + "'";
+      bw.write(solution);
+      bw.newLine();
+    }
+
+    br.close();
+    bw.close();
+  }
+}
